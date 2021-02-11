9587a52a911e2f344746d69736929bfaf96f6df2
Jazmin Solorzano <jazminsol@yahoo.com>
2021-02-11 22:48:04 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,1 +1,14 @@
-# Write your code here
+magic_exit_number = 7
+count = 0
+while count < 10 do
+  break if count == magic_exit_number
+  puts "I am the #{count}, I love to count!"
+  count = count + 1
+end
+
+magic_exit_number = 7
+count = 0
+while count < 10 && count != magic_exit_number do
+  puts "I am the #{count}!"
+  count = count + 1
+end
